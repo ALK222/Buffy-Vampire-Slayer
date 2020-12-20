21cc72f6196197fa7d12ae2f1ac243c3c42deb53
A.L.K <alejandrobarrachina.02@gmail.com>
2020-12-20 18:48:22 +0100

    fix serialize

--- p4/src/control/commands/SerializeCommand.java
+++ p4/src/control/commands/SerializeCommand.java
@@ -7,8 +7,8 @@ import logic.Game;
 public class SerializeCommand extends Command {
 
     private static final String _name = "Serialize";
-    private static final String _shortcut = "C";
-    private static final String _details = "Serialize | C";
+    private static final String _shortcut = "M"; // temporal shortcut
+    private static final String _details = "Serialize | m";
     private static final String _help = "changes the view between serialize and board.";
 
     public SerializeCommand() {
